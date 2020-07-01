e920b5ddb2616e49d77f1e24f0c8e6420e4059f7
_uchanlee <uchanlee.dev@gmail.com>
2020-07-01 19:02:11 +0900

    ✅[CREATE] 07-13.js - Map Collection

---
+++ code_example/[07] ECMAScript 2015(ES6)/src/07-13.js
@@ -0,0 +1,16 @@
+/*
+  맵(Map)은 키-값 쌍의 집합체이며, 키는 고유한 값이어야 한다.
+*/
+
+let teams = new Map();
+teams.set('LG', '트윈스');
+teams.set('삼성', '라이온스');
+teams.set('NC', '다이노스');
+teams.set('기아', '타이거스');
+teams.set('한화', '이글즈');
+teams.set('롯데', '자이언츠');
+
+console.log(teams.has('SK')); // false
+console.log(teams.has('lg')); // false
+console.log(teams.get('lg')); // undefined
+console.log(teams.get('LG')); // 트윈스
