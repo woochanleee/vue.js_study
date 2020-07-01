8f5108d1419a1e6a46eb53165b135c0bc02e9bde
_uchanlee <uchanlee.dev@gmail.com>
2020-07-01 17:19:40 +0900

    ✅[CREATE] 07-09.js - new object literal

---
+++ code_example/[07] ECMAScript 2015(ES6)/build/07-09.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var name = '홍길동';
+var age = 20;
+var email = 'gdhong@test.com';
+
+// var obj = { name: name, age: age, email: email };
+var obj = { name: name, age: age, email: email };
+console.log(obj);
+
+/*
+  ES6(ES2015) 에서는 객체의 속성 표기법이 개선되었다.
+  객체의 속성을 작성할 때 변수명과 동일하다면 생략할 수 있다.
+*/
