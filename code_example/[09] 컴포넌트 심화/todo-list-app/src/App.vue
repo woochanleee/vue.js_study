1e11d87c4616c06ddcca0e68ebb87cc5422ab55e
_uchanlee <uchanlee.dev@gmail.com>
2020-07-02 14:45:57 +0900

    ♻️[UPDATE] <09-01> structure of App.vue

--- code_example/[09] 컴포넌트 심화/todo-list-app/src/App.vue
+++ code_example/[09] 컴포넌트 심화/todo-list-app/src/App.vue
@@ -26,3 +26,10 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<!--
+  전역 컴포넌트와 비교하면 몇 가지 차이점이 있다.
+  <template> 에는 id를 부여하지 않으며, <script> 영역에서는 Vue 컴포넌트의 template을
+  지정하지 않으며, Vue.component() 로 이름과 template 속성을 지정하지 않는다. 반면에 name
+  속성을 지정할 수 있으며, 반드시 객체를 export 해야 한다.
+-->
