c90b525c4db4f7b3dfb5aed92f415383da3eb5c0
_uchanlee <uchanlee.dev@gmail.com>
2020-07-02 22:28:22 +0900

    ✅[CREATE] 09-10 - Child2.vue

---
+++ code_example/[09] 컴포넌트 심화/style-test/src/components/Child2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'child2',
+  data: () => ({ msg: 'Child2' }),
+};
+</script>
+
+<style>
+.main {
+  border: 1px solid black;
+  background-color: aqua;
+}
+</style>
