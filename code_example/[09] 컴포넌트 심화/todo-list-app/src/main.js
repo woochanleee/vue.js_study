2ed83fb4f1e672f6acac41caa7cc3fd449b6a5da
_uchanlee <uchanlee.dev@gmail.com>
2020-07-02 14:54:02 +0900

    ♻️[UPDATE] <09-02> main.js comment

--- code_example/[09] 컴포넌트 심화/todo-list-app/src/main.js
+++ code_example/[09] 컴포넌트 심화/todo-list-app/src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
+
+/*
+  App.vue 컴포넌트를 화면에 보여주기 위해서 main.js를 사용한다
+  App 컴포넌트를 렌더링한 결과물을 #app인 요소에 출력한다.
+*/
