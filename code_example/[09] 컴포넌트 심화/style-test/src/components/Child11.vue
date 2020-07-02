8e12a4fc56fc60a75fc5b6b7a312e53dc84213aa
_uchanlee <uchanlee.dev@gmail.com>
2020-07-02 23:11:07 +0900

    ✅[CREATE] 09-12 - Child11.vue child component scoped style apply

---
+++ code_example/[09] 컴포넌트 심화/style-test/src/components/Child11.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <h3>Child - Child</h3>
+  </div>
+</template>
+
+<style scoped>
+.test {
+  font-style: italic;
+}
+</style>
+
+<!--
+  script태그 안에 export default 안해줘도 작동하네?
+  없어도 기본적으로 알아서 export 해주는건가..?
+-->
