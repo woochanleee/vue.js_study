1cefb01bfe4bd929041751f3d1e66b5c00bee036
_uchanlee <uchanlee.dev@gmail.com>
2020-07-03 08:31:10 +0900

    ♻️[UPDATE]  App.vue css Module

--- code_example/[09] 컴포넌트 심화/style-test/src/App.vue
+++ code_example/[09] 컴포넌트 심화/style-test/src/App.vue
@@ -3,18 +3,21 @@
     <h2>{{ msg }}</h2>
     <child1 />
     <child2 />
+    <module1 />
   </div>
 </template>
 
 <script>
 import Child1 from './components/Child1';
 import Child2 from './components/Child2';
+import Module1 from './components/Module1.vue';
 
 export default {
   name: 'app',
   components: {
     Child1,
     Child2,
+    Module1,
   },
   data: () => ({
     msg: 'Welcome to Your Vue.js App',
