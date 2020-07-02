6d17c6e6a0c23caf2f18d8be86ee92669f588256
_uchanlee <uchanlee.dev@gmail.com>
2020-07-02 20:16:12 +0900

    ✅[CREATE] <09-07> TodoList.vue

---
+++ code_example/[09] 컴포넌트 심화/todo-list-app/src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<style>
+* {
+  box-sizing: border-box;
+}
+header {
+  background-color: purple;
+  padding: 30px 30px;
+  color: yellow;
+  text-align: center;
+}
+header::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<template>
+  <div id="todo-list-app">
+    <header>
+      <h2>Todo List App</h2>
+      <input-todo></input-todo>
+    </header>
+    <list></list>
+  </div>
+</template>
+
+<script>
+import InputTodo from "./InputTodo";
+import List from "./List";
+
+export default {
+  name: "todo-list",
+  components: {
+    InputTodo,
+    List
+  }
+};
+</script>
