15de627e132b1703bc1eac55262cb3d7ddb17c80
_uchanlee <uchanlee.dev@gmail.com>
2020-07-03 20:19:59 +0900

    ✅[CREATE] <09-24> Menu component

---
+++ code_example/[09] 컴포넌트 심화/dynamic-test/src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <h3>{{ new Date().toTimeString() }}</h3>
+  </div>
+</template>
---
+++ code_example/[09] 컴포넌트 심화/dynamic-test/src/components/Contact.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <h3>{{ new Date().toTimeString() }}</h3>
+  </div>
+</template>
