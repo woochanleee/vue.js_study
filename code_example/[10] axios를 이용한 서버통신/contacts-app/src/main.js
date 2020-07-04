2767c37e33781c5e4ea772da2e04f4e2683e2b92
_uchanlee <uchanlee.dev@gmail.com>
2020-07-04 15:55:16 +0900

    ♻️[UPDATE] <contact example> pollyfill and bootstrap

--- code_example/[10] axios를 이용한 서버통신/contacts-app/src/main.js
+++ code_example/[10] axios를 이용한 서버통신/contacts-app/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 // import App from './AppAxiosTest';
 import axios from 'axios';
+import 'bootstrap/dist/bootstrap.min.css';
+import ES6Promise from 'es6-promise';
+
+ES6Promise.polyfill();
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
