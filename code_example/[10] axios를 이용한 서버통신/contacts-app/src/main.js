7bfcbbe68d354464fd6d7967fe50e5e8c37b6a1b
_uchanlee <uchanlee.dev@gmail.com>
2020-07-04 14:51:14 +0900

    ♻️[UPDATE] <10-11> main.js use axios in Vue instance (Vue.prototype)

--- code_example/[10] axios를 이용한 서버통신/contacts-app/src/main.js
+++ code_example/[10] axios를 이용한 서버통신/contacts-app/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 // import App from './App.vue'
 import App from './AppAxiosTest';
+import axios from 'axios';
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+/*
+  Vue 인스턴스 내부에서 axios를 이용하기 위해 Vue.prototype에 axios를 추가하면 더욱 간단하게 사용할 수 있다.
+  
+  이제 Vue 인스턴스 내부에서는 axios를 import 하지 않고도 this.$axios 를 사용할 수 있다.
+*/
