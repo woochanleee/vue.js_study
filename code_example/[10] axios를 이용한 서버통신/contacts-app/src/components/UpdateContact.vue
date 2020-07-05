b427f59297a04691e66ffb5871ef8eb79180f52c
_uchanlee <uchanlee.dev@gmail.com>
2020-07-05 14:22:23 +0900

    ♻️[UPDATE] <10-22> UpdateContact.vue

---
+++ code_example/[10] axios를 이용한 서버통신/contacts-app/src/components/UpdateContact.vue
@@ -0,0 +1,15 @@
+<template>
+  <contactForm mode="update" :contact="contact" />
+</template>
+
+<script>
+import ContactForm from './ContactForm';
+
+export default {
+  name: 'updateContact',
+  components: {
+    ContactForm,
+  },
+  props: ['contact'],
+};
+</script>
