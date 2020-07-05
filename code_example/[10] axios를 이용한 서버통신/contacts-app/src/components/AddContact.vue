6cfa36d377946fc16a9009114338e57e1a2ba334
_uchanlee <uchanlee.dev@gmail.com>
2020-07-05 14:22:13 +0900

    ♻️[UPDATE] <10-21> AddContact.vue

---
+++ code_example/[10] axios를 이용한 서버통신/contacts-app/src/components/AddContact.vue
@@ -0,0 +1,14 @@
+<template>
+  <contactForm mode="add" />
+</template>
+
+<script>
+import ContactForm from './ContactForm';
+
+export default {
+  name: 'addContact',
+  components: {
+    ContactForm,
+  },
+};
+</script>
