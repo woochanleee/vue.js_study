271ea6c9de4be0593b313a75fd2815b1b1701421
_uchanlee <uchanlee.dev@gmail.com>
2020-07-10 11:02:56 +0900

    ♻️[UPDATE] <11-03> src/main.js add store in Vue instance

--- code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/main.js
+++ code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 import TodoList from './components/TodoList';
+import store from './store';
 
 Vue.config.devtools = true;
+Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(TodoList),
 }).$mount('#app');
+
+/*
+  Vue.use(vuex) 와 new Vue({ store }) 를 통해
+  this.$store로 저장소(store) 객체를 접근할수 있다.
+*/
