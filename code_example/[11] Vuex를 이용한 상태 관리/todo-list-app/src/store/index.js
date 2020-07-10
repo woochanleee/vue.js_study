77d8bb30cad2b9839b853f15bf6cf0995135858d
_uchanlee <uchanlee.dev@gmail.com>
2020-07-10 10:54:01 +0900

    ✅[CREATE] <11-02> store/index.js

---
+++ code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/store/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Constant from '../Constant';
+
+Vue.use(vuex);
+
+const store = new Vuex.store({
+  state: {
+    todoList: [
+      {
+        id: 1,
+        todo: '영화보기',
+        done: false,
+      },
+      {
+        id: 2,
+        todo: '주말 산책',
+        done: true,
+      },
+      {
+        id: 3,
+        todo: 'ES6 학습',
+        done: false,
+      },
+      {
+        id: 4,
+        todo: '잠실 야구장',
+        done: false,
+      },
+    ],
+  },
+  mutations: {
+    [Constant.ADD_TODO]: (state, payload) => {
+      if (payload.todo !== '') {
+        state.todoList.push(
+          // 리덕스와는 다르게 불변성을 유지하지 않고 기존거에 추가를 하네?
+          {
+            id: new Date().getTime(),
+            todo: payload.todo,
+            done: false,
+          }
+        );
+      }
+    },
+    [Constant.DONE_TOGGLE]: (state, payload) => {
+      const index = state.todoList.findIndex((item) => item.id === payload.id);
+      state.todoList[index].done = !state.todoList[index].done;
+    },
+    [Constant.DELETE_TOTO]: (state, payload) => {
+      const index = state.todoList.findIndex((item) => item.id === payload.id);
+      state.todoList.splice(index, 1);
+    },
+  },
+});
+
+export default store;
