21de36419614c05b26c22f50f03ce85b7d657791
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 08:29:10 +0900

    ✅[CREATE] <11-20> Search.vue

---
+++ code_example/[11] Vuex를 이용한 상태 관리/contacts-app-search/src/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>
+    이름 :
+    <input
+      type="text"
+      v-model.trim="name"
+      placeholder="두 글자 이상 입력 후 엔터!"
+      @keyup.enter="keyupEvent"
+    />
+  </p>
+</template>
+
+<script>
+import Constant from '../Constant';
+
+export default {
+  name: 'search',
+  data: () => ({
+    name: '',
+  }),
+  methods: {
+    keyupEvent(e) {
+      const value = e.target.value;
+      if (value.length >= 2) {
+        this.$store.dispatch(Constant.SEARCH_CONTACT, { name: value });
+        this.name = '';
+      } else {
+        this.$store.dispatch(Constant.SEARCH_CONTACT, { name: '' });
+      }
+    },
+  },
+};
+</script>
