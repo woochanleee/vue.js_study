400c79b8f109ce44d01ddfc65a5aab019ff5f428
_uchanlee <uchanlee.dev@gmail.com>
2020-07-14 22:12:08 +0900

    ♻️[UPDATE] <11-13> store/index.js
    add actions

--- code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/store/index.js
+++ code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/store/index.js
@@ -51,6 +51,20 @@ const store = new Vuex.Store({
       state.todoList.splice(index, 1);
     },
   },
+  actions: {
+    [Constant.ADD_TODO]: (store, payload) => {
+      console.log('###addTodo!!!', payload);
+      store.commit(Constant.ADD_TODO, payload);
+    },
+    [Constant.DELETE_TOTO]: (store, payload) => {
+      console.log('###deleteTodo!!!', payload);
+      store.commit(Constant.DELETE_TOTO, payload);
+    },
+    [Constant.DONE_TOGGLE]: ({ state, commit }, payload) => {
+      console.log('###doneToglle!!!', payload);
+      commit(Constant.DONE_TOGGLE, payload);
+    },
+  },
 });
 
 export default store;
