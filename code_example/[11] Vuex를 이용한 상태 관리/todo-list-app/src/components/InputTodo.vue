0bc664f2509382b5715d2a00f1c2ac1f7b0cdaee
_uchanlee <uchanlee.dev@gmail.com>
2020-07-14 22:22:37 +0900

    ♻️[UPDATE] <11-14> InputTodo.vue
    apply dispatch

--- code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/components/InputTodo.vue
+++ code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/components/InputTodo.vue
@@ -40,10 +40,14 @@ export default {
   data: () => ({ todo: "" }),
   methods: {
     addTodo() {
+      // 아? 약간 action이 리덕스 미들웨어랑 비슷한 역할인가? action에서 store.commit하는거 보면 약간 그렇게 보이긴 하는데
+      this.$store.dispatch(Constant.ADD_TODO, { todo: this.todo }); // dispatch는 commit(뮤테이션 할때랑)이랑 거의 똑같네
+
       // mutation을 발생할때 store.commit 이라는걸 쓰는구나!
-      this.$store.commit(Constant.ADD_TODO, {
-        todo: this.todo
-      });
+
+      // this.$store.commit(Constant.ADD_TODO, {
+      //   todo: this.todo
+      // });
     }
   }
 };
