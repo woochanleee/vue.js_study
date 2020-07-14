4b8948f06ff81280f6492b43ccbb9983978cafec
_uchanlee <uchanlee.dev@gmail.com>
2020-07-14 21:59:00 +0900

    ✅[CREATE] <11-11> RegionButtons.vue

---
+++ code_example/[11] Vuex를 이용한 상태 관리/country-app/src/components/RegionButtons.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <button class="region" v-for="region in regions" :key="region"
+    v-bind:class="isSelected(region)"
+    @click="changeRegion({ region: region })">
+    {{ region }}
+    </button>
+  </div>
+</template>
+
+<script>
+import Constant from '../Constant';
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'RegionButtons',
+  computed: {
+    regions() {
+      return this.$store.getters.regions;
+    },
+    currentRegion() {
+      return this.$store.getters.currentRegion;
+    }
+  },
+  methods: {
+    isSelected(region) {
+      if (region === this.currentRegion) return { selected: true };
+      else return { selected: false }
+    },
+    ...mapMutations([
+      Constant.CHANGE_REGION
+    ])
+  }
+}
+</script>
+
+<style>
+  button.region {
+    text-align: center;
+    width: 80px;
+    margin: 2px;
+    border: 1px solid gray;
+  }
+  button.selected {
+    background-color: purple;
+    color: aqua;
+  }
+</style>
