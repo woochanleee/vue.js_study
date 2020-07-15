9d05ae4978bc03545289f7261746c6d0900ca64f
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 13:37:23 +0900

    ♻️[UPDATE] <11-25> store/index.js

--- code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/store/index.js
+++ code_example/[11] Vuex를 이용한 상태 관리/todo-list-app/src/store/index.js
@@ -1,70 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Constant from '../Constant';
+import state from './state';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    todoList: [
-      {
-        id: 1,
-        todo: '영화보기',
-        done: false,
-      },
-      {
-        id: 2,
-        todo: '주말 산책',
-        done: true,
-      },
-      {
-        id: 3,
-        todo: 'ES6 학습',
-        done: false,
-      },
-      {
-        id: 4,
-        todo: '잠실 야구장',
-        done: false,
-      },
-    ],
-  },
-  mutations: {
-    [Constant.ADD_TODO]: (state, payload) => {
-      if (payload.todo !== '') {
-        state.todoList.push(
-          // 리덕스와는 다르게 불변성을 유지하지 않고 기존거에 추가를 하네?
-          {
-            id: new Date().getTime(),
-            todo: payload.todo,
-            done: false,
-          }
-        );
-      }
-    },
-    [Constant.DONE_TOGGLE]: (state, payload) => {
-      const index = state.todoList.findIndex((item) => item.id === payload.id);
-      state.todoList[index].done = !state.todoList[index].done;
-    },
-    [Constant.DELETE_TOTO]: (state, payload) => {
-      const index = state.todoList.findIndex((item) => item.id === payload.id);
-      state.todoList.splice(index, 1);
-    },
-  },
-  actions: {
-    [Constant.ADD_TODO]: (store, payload) => {
-      console.log('###addTodo!!!', payload);
-      store.commit(Constant.ADD_TODO, payload);
-    },
-    [Constant.DELETE_TOTO]: (store, payload) => {
-      console.log('###deleteTodo!!!', payload);
-      store.commit(Constant.DELETE_TOTO, payload);
-    },
-    [Constant.DONE_TOGGLE]: ({ commit }, payload) => {
-      console.log('###doneToglle!!!', payload);
-      commit(Constant.DONE_TOGGLE, payload);
-    },
-  },
+  state,
+  mutations,
+  actions,
 });
 
 export default store;
