f1675b20864c19fa87e330b6da6f5cf048fb6a26
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 13:59:14 +0900

    ♻️[UPDATE] <11-28> store/index.js
    add module

--- code_example/[11] Vuex를 이용한 상태 관리/contacts-app-search/src/store/index.js
+++ code_example/[11] Vuex를 이용한 상태 관리/contacts-app-search/src/store/index.js
@@ -1,25 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Constant from '../Constant';
-import axios from 'axios';
+import module1 from './module1';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    contacts: [],
+    keywords: [],
   },
   mutations: {
-    [Constant.SEARCH_CONTACT]: (state, payload) => {
-      state.contacts = payload.contacts;
+    [Constant.ADD_KEYWORD]: (state, payload) => {
+      state.keywords.splice(0, 0, payload.name);
     },
   },
   actions: {
-    [Constant.SEARCH_CONTACT]: (store, payload) => {
-      axios.get(Constant.BASE_URL + payload.name).then((res) => {
-        store.commit(Constant.SEARCH_CONTACT, { contacts: res.data });
-      });
+    [Constant.ADD_KEYWORD]: (store, payload) => {
+      store.commit(Constant.ADD_KEYWORD, payload);
     },
+  },
+  modules: {
+    m1: module1,
   },
 });
 
