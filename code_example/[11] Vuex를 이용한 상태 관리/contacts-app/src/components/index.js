630bb5f4014e5dd3720dd701cd43c623d5764ba9
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 16:07:19 +0900

    ✅[CREATE] <ContactsApp> project setting with vuex

---
+++ code_example/[11] Vuex를 이용한 상태 관리/contacts-app/src/components/index.js
@@ -0,0 +1,6 @@
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import UpdateContact from './UpdateContact';
+import UpdatePhoto from './UpdatePhoto';
+
+export { AddContact, ContactList, UpdateContact, UpdatePhoto };
