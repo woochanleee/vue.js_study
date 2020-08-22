0b26b52460e196face7a0dbfeb6aa46800b68458
_uchanlee <uchanlee.dev@gmail.com>
2020-08-22 15:50:37 +0900

    ♻️[UPDATE] <12-27> mutation.js

--- code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/mutations.js
+++ code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/mutations.js
@@ -1,31 +1,20 @@
 import Constant from '../Constant';
 
-// 상태를 변경하는 기능만 구현합니다.
+// 상태를 변경하는 기능만을 뽑아서...
 export default {
-  [Constant.ADD_CONTACT_FORM]: (state) => {
+  [Constant.FETCH_CONTACTS]: (state, payload) => {
+    state.contacts = payload.contacts;
+  },
+  [Constant.FETCH_CONTACT_ONE]: (state, payload) => {
+    state.contact = payload.contact;
+  },
+  [Constant.INITIALIZE_CONTACT_ONE]: (state) => {
     state.contact = {
       no: '',
       name: '',
       tel: '',
       address: '',
       photo: '',
-    };
-    state.mode = 'add';
-    state.currentView = 'contactForm';
-  },
-  [Constant.CANCEL_FORM]: (state) => {
-    state.currentView = null;
-  },
-  [Constant.EDIT_CONTACT_FORM]: (state, payload) => {
-    state.contact = payload.contact;
-    state.mode = 'update';
-    state.currentView = 'contactForm';
-  },
-  [Constant.EDIT_PHOTO_FORM]: (state, payload) => {
-    state.contact = payload.contact;
-    state.currentView = 'updatePhoto';
-  },
-  [Constant.FETCH_CONTACTS]: (state, payload) => {
-    state.contacts = payload.contacts;
-  },
+    }
+  }
 };
