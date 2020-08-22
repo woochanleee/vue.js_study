4892f7a9ffcd9d37f721a97e1749230e2a45f5ec
_uchanlee <uchanlee.dev@gmail.com>
2020-08-22 12:42:29 +0900

    ♻️[UPDATE] <12-25> Constant.js
    화면 관련 상수 제거

--- code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/Constant.js
+++ code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/Constant.js
@@ -1,12 +1,10 @@
 export default {
   // 변이와 액션 모두에서 사용
-  ADD_CONTACT_FORM: 'addContactForm', // 입력폼 나타내기
-  CANCEL_FORM: 'cancelForm', // 입력, 수정폼 닫기
-  EDIT_CONTACT_FORM: 'editContactForm', // 수정폼 나타내기
-  EDIT_PHOTO_FORM: 'editPhotoForm', // 사진 편집폼 나타내기
   FETCH_CONTACTS: 'fetchContacts', // 연락처 조회
+  FETCH_CONTACT_ONE: 'fetchContactOne', // 연락처 한건 조회
+  INITIALIZE_CONTACT_ONE: 'initializeContactOne', // 연락처 초기화
 
-  // 액션에서만 사용
+  // 액션만 존재
   ADD_CONTACT: 'addContact', // 연락처 추가
   UPDATE_CONTACT: 'updateContact', // 연락처 수정
   UPDATE_PHOTO: 'updatePhoto', // 사진 수정
