1e71b5ffba589f7861a5ddafe620966afd020675
_uchanlee <uchanlee.dev@gmail.com>
2020-08-22 16:23:27 +0900

    ♻️[UPDATE] <12-28> actions.js

--- code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/actions.js
+++ code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/actions.js
@@ -3,50 +3,6 @@ import axios from 'axios';
 import CONF from '../Config';
 
 export default {
-  [Constant.ADD_CONTACT_FORM]: (store) => {
-    store.commit(Constant.ADD_CONTACT_FORM);
-  },
-  [Constant.ADD_CONTACT]: (store) => {
-    axios.post(CONF.ADD, store.state.contact).then((res) => {
-      if (res.data.status === 'success') {
-        store.dispatch(Constant.CANCEL_FORM);
-        store.dispatch(Constant.FETCH_CONTACTS, { pageNo: 1 });
-      } else {
-        console.log('연락처 추가 실패: ' + res.data);
-      }
-    });
-  },
-  [Constant.EDIT_CONTACT_FORM]: (store, payload) => {
-    axios.get(CONF.FETCH_ONE(payload.no)).then((res) => {
-      store.commit(Constant.EDIT_CONTACT_FORM, { contact: res.data });
-    });
-  },
-  [Constant.UPDATE_CONTACT]: (store) => {
-    const currentPageNo = store.state.contacts.pageNo;
-    const contact = store.state.contact;
-    axios.put(CONF.UPDATE(contact.no), contact).then((res) => {
-      if (res.data.status === 'success') {
-        store.dispatch(Constant.CANCEL_FORM);
-        store.dispatch(Constant.FETCH_CONTACTS, { pageNo: currentPageNo });
-      } else {
-        console.log('연락처 변경 실패: ' + res.data);
-      }
-    });
-  },
-  [Constant.EDIT_PHOTO_FORM]: (store, payload) => {
-    axios.get(CONF.FETCH_ONE(payload.no)).then((res) => {
-      store.commit(Constant.EDIT_PHOTO_FORM, { contact: res.data });
-    });
-  },
-  [Constant.UPDATE_PHOTO]: (store, payload) => {
-    const currentPageNo = store.state.contacts.pageNo;
-    const data = new FormData();
-    data.append('photo', payload.file);
-    axios.post(CONF.UPDATE_PHOTO(payload.no), data).then(() => {
-      store.dispatch(Constant.CANCEL_FORM);
-      store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
-    });
-  },
   [Constant.FETCH_CONTACTS]: (store, payload) => {
     let pageNo;
     if (typeof payload === 'undefined' || typeof payload.pageNo === 'undefined')
@@ -67,8 +23,33 @@ export default {
         });
       });
   },
-  [Constant.CANCEL_FORM]: (store) => {
-    store.commit(Constant.CANCEL_FORM);
+  [Constant.ADD_CONTACT]: (store) => {
+    axios.post(CONF.ADD, store.state.contact).then((res) => {
+      if (res.data.status === 'success') {
+        store.dispatch(Constant.FETCH_CONTACTS, { pageNo: 1 });
+      } else {
+        console.log('연락처 추가 실패: ' + res.data);
+      }
+    });
+  },
+  [Constant.UPDATE_CONTACT]: (store) => {
+    const currentPageNo = store.state.contacts.pageNo;
+    const contact = store.state.contact;
+    axios.put(CONF.UPDATE(contact.no), contact).then((res) => {
+      if (res.data.status === 'success') {
+        store.dispatch(Constant.FETCH_CONTACTS, { pageNo: currentPageNo });
+      } else {
+        console.log('연락처 변경 실패: ' + res.data);
+      }
+    });
+  },
+  [Constant.UPDATE_PHOTO]: (store, payload) => {
+    const currentPageNo = store.state.contacts.pageNo;
+    const data = new FormData();
+    data.append('photo', payload.file);
+    axios.post(CONF.UPDATE_PHOTO(payload.no), data).then(() => {
+      store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+    });
   },
   [Constant.DELETE_CONTACT]: (store, payload) => {
     const currentPageNo = store.state.contacts.pageNo;
@@ -76,4 +57,12 @@ export default {
       store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
     });
   },
+  [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
+    axios.get(CONF.FETCH_ONE(payload.no)).then(res => {
+      store.commit(Constant.FETCH_CONTACT_ONE, { contact: res.data });
+    })
+  },
+  [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
+    store.commit(Constant.INITIALIZE_CONTACT_ONE);
+  }
 };
