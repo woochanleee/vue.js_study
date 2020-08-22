36c817a8a0905e218f85fe099a8fe2eb50f80e6a
_uchanlee <uchanlee.dev@gmail.com>
2020-08-22 17:30:21 +0900

    ♻️[UPDATE] <12-30> App.vue

--- code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/App.vue
+++ code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/App.vue
@@ -2,25 +2,20 @@
   <div id="container">
     <div class="page-header">
       <h1 class="text-center">연락처 관리 애플리케이션</h1>
-      <p>(Dynamic Component + EventBus + Axios)</p>
+      <p>(Vue-router + Vuex + Axios)</p>
+      <div class="btn-group">
+        <router-link to="/home" class="btn btn-info menu">Home</router-link>
+        <router-link to="/about" class="btn btn-info menu">About</router-link>
+        <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
+      </div>
     </div>
-    <component :is="currentView"></component>
-    <contactList />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { ContactList, ContactForm, UpdatePhoto } from './components';
-import { mapState } from 'vuex';
-
 export default {
   name: 'app',
-  components: {
-    ContactList,
-    ContactForm,
-    UpdatePhoto,
-  },
-  computed: mapState(['currentView']),
 };
 </script>
 
