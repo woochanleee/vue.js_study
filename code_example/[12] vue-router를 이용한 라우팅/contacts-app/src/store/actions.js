19537cd7542b2b181aeb5e09cfb39eb9e294a40a
_uchanlee <uchanlee.dev@gmail.com>
2020-08-22 20:31:31 +0900

    ♻️[UPDATE] <12-39> actions.js

--- code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/actions.js
+++ code_example/[12] vue-router를 이용한 라우팅/contacts-app/src/store/actions.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 import CONF from '../Config';
 
 export default {
+  [Constant.CHANGE_ISLOADING]: (store, payload) => {
+    store.commit(Constant.CHANGE_ISLOADING, payload);
+  },
   [Constant.FETCH_CONTACTS]: (store, payload) => {
     let pageNo;
     if (typeof payload === 'undefined' || typeof payload.pageNo === 'undefined')
       pageNo = 1;
     else pageNo = payload.pageNo;
     const pageSize = store.state.contacts.pageSize;
-
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
     axios
       .get(CONF.FETCH, {
         params: {
@@ -21,9 +24,11 @@ export default {
         store.commit(Constant.FETCH_CONTACTS, {
           contacts: res.data,
         });
+        store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: false });
       });
   },
   [Constant.ADD_CONTACT]: (store) => {
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
     axios.post(CONF.ADD, store.state.contact).then((res) => {
       if (res.data.status === 'success') {
         store.dispatch(Constant.FETCH_CONTACTS, { pageNo: 1 });
@@ -33,6 +38,7 @@ export default {
     });
   },
   [Constant.UPDATE_CONTACT]: (store) => {
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
     const currentPageNo = store.state.contacts.pageno;
     const contact = store.state.contact;
     axios.put(CONF.UPDATE(contact.no), contact).then((res) => {
@@ -44,6 +50,7 @@ export default {
     });
   },
   [Constant.UPDATE_PHOTO]: (store, payload) => {
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
     const currentPageNo = store.state.contacts.pageNo;
     const data = new FormData();
     data.append('photo', payload.file);
@@ -52,17 +59,20 @@ export default {
     });
   },
   [Constant.DELETE_CONTACT]: (store, payload) => {
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
     const currentPageNo = store.state.contacts.pageNo;
     axios.delete(CONF.DELETE(payload.no)).then(() => {
       store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
     });
   },
   [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
-    axios.get(CONF.FETCH_ONE(payload.no)).then(res => {
+    store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: true });
+    axios.get(CONF.FETCH_ONE(payload.no)).then((res) => {
       store.commit(Constant.FETCH_CONTACT_ONE, { contact: res.data });
-    })
+      store.dispatch(Constant.CHANGE_ISLOADING, { isLoading: false });
+    });
   },
   [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
     store.commit(Constant.INITIALIZE_CONTACT_ONE);
-  }
+  },
 };
