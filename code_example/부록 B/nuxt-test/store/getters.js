df18c0352aaa8985bc1100760401b12b03feddfa
_uchanlee <uchanlee.dev@gmail.com>
2020-08-24 14:51:47 +0900

    ✅[CREATE] <B-05> getters.js

---
+++ code_example/부록 B/nuxt-test/store/getters.js
@@ -0,0 +1,8 @@
+export default {
+  getContactOne(state) {
+    const no = state.no;
+    const array = state.contacts.filter((item) => item.no === no);
+    if (array.length === 1) return array[0];
+    else return {};
+  },
+};
